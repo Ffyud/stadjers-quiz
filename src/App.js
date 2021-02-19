91072b1e9441a616e9521e966563de1cd02fa770
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-02-19 19:08:38 +0100

    onzin css magie weg

--- src/App.js
+++ src/App.js
@@ -48,20 +48,6 @@ class App extends Component {
       return (
         <div className="App">
           <div role="dialog" className="intro" >
-          {/* <div className="floating-question">Bernoulliplein</div>
-          <div className="floating-question">Grote Markt</div>
-          <div className="floating-question">Bommen Berend</div>
-          <div className="floating-question">Noorderplantsoen</div>
-          <div className="floating-question">Pioenpark</div>
-          <div className="floating-question">Vinkhuizen</div>
-          <div className="floating-question">Kardinge</div>
-          <div className="floating-question">Stadspark</div>
-          <div className="floating-question">Vismarkt</div>
-          <div className="floating-question">Oosterkerk</div>
-          <div className="floating-question">KEI-week</div>
-          <div className="floating-question">Sint-Joris</div>
-          <div className="floating-question">Stadshuis</div>
-          <div className="floating-question">Beijum</div> */}
             <div className="introText">
             <h1>Welkom bij de <span className='title'>Stadjers Quiz! &#x1F44B; </span></h1>
             Beantwoord 25 vragen over Stad Groningen en laat zien dat jij een kenner bent. <br/><br/>Er zijn in totaal {quizData.length} vragen, dus speel gerust nog eens.<br/><br/>
