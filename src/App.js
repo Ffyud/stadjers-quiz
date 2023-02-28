234f6a8baaa8f6c28bcd5b76d4ae694581ffca9a
David <5263778+Ffyud@users.noreply.github.com>
2023-02-28 11:33:08 +0100

    update to functional component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
-import QuizVragen from './QuizVragen';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import QuizVragen from "./QuizVragen";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vanStart: false
-    };
-  }
+function App() {
+    const [start, setStart] = useState(false);
 
-  vanStart = (event) => {
-    this.setState({
-      vanStart: true
-    });
-  }
-
-  vanStartEnter = (event) => {
-    if (event.key === 'Enter') {
-      this.setState({
-        vanStart: true
-      });
-    }
-  }
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.vanStartEnter);
-
-    console.log('------------------------');
-    console.log('                        ');
-    console.log('Welkom bij deze quiz!   ');
-    console.log('Hier loggen we wat info.');
-    console.log('------------------------');
-    console.log('https://github.com/Ffyud/stadjers-quiz');
-  }
-
-  render() {
-    if(this.state.vanStart === true) {
-      return (
-        <div className="App">
-          <QuizVragen/>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div className="App">
-          <div role="dialog" className="intro" >
-            <div className="introText">
-            <h1>Welkom bij de <span className='title'>Stadjers Quiz! &#x1F44B; </span></h1>
-            <span className='sub-title'>Beantwoord 25 vragen over Stad Groningen en laat zien dat jij een kenner bent.</span>
-            </div>
-            <div className="introStart" onClick={this.vanStart}>
-              <div className="start">START &#128073;</div>
-            </div>
-           
-            <div className="deelMe">
-            Deel deze quiz vooral: <a href="https://stadjers-quiz.nl">stadjers-quiz.nl</a>
-            </div>
-          </div>
-        </div>
-      );
+    const vanStart = () => {
+        console.log("Start de quiz!")
+        setStart(true);
     }
 
-  }
+    if (start === true) {
+        return (
+            <div className="App">
+                <QuizVragen />
+            </div>
+        );
+    } else {
+        return (
+            <div className="App">
+                <div role="dialog" className="intro">
+                    <div className="introText">
+                        <h1>
+                            Welkom bij de <span className="title">Stadjers Quiz! &#x1F44B; </span>
+                        </h1>
+                        <span className="sub-title">
+                            Beantwoord 25 vragen over Stad Groningen en laat zien dat jij een kenner bent.
+                        </span>
+                    </div>
+                    <div className="introStart" onClick={() => vanStart()}>
+                        <div className="start">START &#128073;</div>
+                    </div>
+
+                    <div className="deelMe">
+                        Deel deze quiz vooral: <a href="https://stadjers-quiz.nl">stadjers-quiz.nl</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
